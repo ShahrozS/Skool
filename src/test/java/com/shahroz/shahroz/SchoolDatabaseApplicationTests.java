0ae3d775c8ff5cf3d767efb6af6b15d48f321e7f
ShahrozS <shahrozsalman7@gmail.com>
2025-07-29 21:19:31 +0500

    initialization of the project and github repo

---
+++ src/test/java/com/shahroz/shahroz/SchoolDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shahroz.shahroz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
